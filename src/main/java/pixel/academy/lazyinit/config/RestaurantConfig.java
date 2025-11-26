fef0eada449ed1be282d5268ad666e390f65bd25
VictorDemerji <demerji.v1@gmail.com>
2025-11-26 18:34:41 +0200

    feat: add bean cofiguration class

---
+++ src/main/java/pixel/academy/lazyinit/config/RestaurantConfig.java
@@ -0,0 +1,15 @@
+package pixel.academy.lazyinit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pixel.academy.lazyinit.common.Chef;
+import pixel.academy.lazyinit.common.GreekChef;
+
+@Configuration
+public class RestaurantConfig {
+
+    @Bean
+    public Chef greekChef() {
+        return new GreekChef();
+    }
+}
